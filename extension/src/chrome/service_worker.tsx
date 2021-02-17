95c4f9041b8444c7d0203c23d407e1c48e463af1
rafaavc <rafavc2508@gmail.com>
2021-02-17 08:48:08 +0000

    Data retrieval performance improvements

--- extension/src/chrome/service_worker.tsx
+++ extension/src/chrome/service_worker.tsx
@@ -4,8 +4,6 @@ import { Message } from "../utils/Message";
 const LIFE_EXPECTANCY_MINS = 20
 const REAPER_INTERVAL_SECS = 30
 
-// TODO speed improvements by saving the current tab (by having a tabs.onActivated event listener)
-
 interface TabData {
     id: number,
     timestamp: number,
@@ -18,8 +16,15 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log("Service worker installed.")
 });
 
+let currentTabId: number = -1
+
 const getCurrentTab = (callback: Function) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => { callback(tabs[0]) })
+    if (currentTabId === -1) {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => { 
+            if (tabs[0].id) callback(tabs[0].id) 
+            else logEvent("Tried to query for active tab but received tab without id. Tabs:", tabs)
+        })
+    } else callback(currentTabId)
 }
 
 /**
@@ -47,6 +52,12 @@ const addTabData = (id: number, data: any): void => {
     else tabs.push({ id: id, timestamp: performance.now(), data: data })
 }
 
+const removeTabData = (id: number) => {
+    const tab = tabs.find((tab) => tab.id === id)
+    if (!tab) return
+    tabs.splice(tabs.indexOf(tab), 1)
+}
+
 const clearCache = () => {
     const now = performance.now()
     for (let i = tabs.length - 1; i >= 0; i--) {
@@ -59,6 +70,15 @@ const clearCache = () => {
 }
 
 setInterval(clearCache, REAPER_INTERVAL_SECS*1000)
+
+chrome.tabs.onUpdated.addListener((tabId: number, changeInfo: chrome.tabs.TabChangeInfo) => {
+    if (changeInfo.url) removeTabData(tabId)    // if the url has changed, forget what was stored
+})
+
+chrome.tabs.onActivated.addListener((activeInfo: chrome.tabs.TabActiveInfo) => {
+    currentTabId = activeInfo.tabId    // keep track of the current tab so that there's no need to constantly query the tabs
+                                       // this makes it very fast to retrieve the data related to the tab
+})
 
 const processTabContentData = (content: any, tabId: number, extender?: Function) => {
     fetch(`${process.env.REACT_APP_SERVER_URL}/extension/api`, {
@@ -94,14 +114,8 @@ chrome.runtime.onMessage.addListener((message: Message, sender: chrome.runtime.M
 
         // check what is the active page and retrieve the info that corresponds to it
         // if none is found, request info from the content script
-        getCurrentTab((tab: chrome.tabs.Tab) => {
-            if (tab.id == undefined) {
-                sendResponse("invalid_tab")
-                return
-            }
-            const data = getTabData(tab.id)
-
-            logEvent("get_ui_data:", tab, tab.id, data)
+        getCurrentTab((tabId: number) => {
+            const data = getTabData(tabId)
 
             if (data) {
                 logEvent("sent data", data)
@@ -109,9 +123,8 @@ chrome.runtime.onMessage.addListener((message: Message, sender: chrome.runtime.M
             }
             else {
                 logEvent("going to get the data")
-                chrome.tabs.sendMessage(tab.id, { type: "get_page_info" }, (message: any) => {
-                    if (!tab.id) return
-                    processTabContentData(message, tab.id, (processed: any) => { sendResponse(processed) })
+                chrome.tabs.sendMessage(tabId, { type: "get_page_info" }, (message: any) => {
+                    processTabContentData(message, tabId, (processed: any) => { sendResponse(processed) })
                 })
             }
 
